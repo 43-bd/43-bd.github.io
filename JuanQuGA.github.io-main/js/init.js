f1932ddcf381d31f20acb865d112fe7fdb5a403b
43-bd <82480344+43-bd@users.noreply.github.com>
2021-04-13 19:33:31 -0500

    Add files via upload

---
+++ JuanQuGA.github.io-main/js/init.js
@@ -0,0 +1,19 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+     apiKey: "AIzaSyBWQRgzdMQe9BCea1MwqOQ2tRjVFgmyaRw",
+    authDomain: "adrianqugaic43.firebaseapp.com",
+    projectId: "adrianqugaic43",
+    storageBucket: "adrianqugaic43.appspot.com",
+    messagingSenderId: "2079137743",
+    appId: "1:2079137743:web:9660bf1f70d2e8ea49bcba",
+    measurementId: "G-YXR80DQ2TM"
+
+});
