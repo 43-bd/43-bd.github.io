52a4528e788b05b9c36e2b83358c689068760fa7
43-bd <82480344+43-bd@users.noreply.github.com>
2021-04-14 00:13:47 -0500

    Update init.js

--- JuanQuGA.github.io-main/js/init.js
+++ JuanQuGA.github.io-main/js/init.js
@@ -8,11 +8,5 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-     apiKey: "AIzaSyDKUMt0m1KThP6-IOGaHk9s0xb2KHI6iZE",
-    authDomain: "bd43-76e55.firebaseapp.com",
-    projectId: "bd43-76e55",
-    storageBucket: "bd43-76e55.appspot.com",
-    messagingSenderId: "135043009259",
-    appId: "1:135043009259:web:f46f2cbb0ce22c369c0cae",
-    measurementId: "G-FC8F0FQ7ZG"
+     
 });
